09e97495a55ba335b429cc1c5e8cfad75fe1110a
RhodyArthur <rhodaarthur48@gmail.com>
2025-11-18 11:15:38 +0000

    feat: implement main function for user prompt processing and content generation

--- main.py
+++ main.py
@@ -1,5 +1,44 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+
+
 def main():
-    print("Hello from ai-agent!")
+
+    verbose = "--verbose" in sys.argv
+
+    args = [arg for arg in sys.argv[1:] if arg != "--verbose"]
+
+    if len(args) != 1:
+        print("Provide a prompt")
+        sys.exit(1)
+
+    user_prompt = args[0]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
+    # create a new instance of a Gemini client
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents=messages
+    )
+
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print(response.text)
 
 
 if __name__ == "__main__":
