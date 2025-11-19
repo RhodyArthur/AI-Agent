5dca7fbe029d198e2257c8293d310911158ff2a3
RhodyArthur <rhodaarthur48@gmail.com>
2025-11-19 12:17:04 +0000

    feat: enhance main.py with usage instructions and integrate system prompt

--- main.py
+++ main.py
@@ -1,44 +1,61 @@
 import os
 import sys
-from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from dotenv import load_dotenv
+from prompts import system_prompt
+from call_function import available_functions
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
 
 
 def main():
+    load_dotenv()
 
     verbose = "--verbose" in sys.argv
-
     args = [arg for arg in sys.argv[1:] if arg != "--verbose"]
 
     if len(args) != 1:
-        print("Provide a prompt")
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
         sys.exit(1)
 
+    api_key = os.environ.get("GEMINI_API_KEY")
+    # create a new instance of a Gemini client
+    client = genai.Client(api_key=api_key)
     user_prompt = args[0]
+
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
 
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    # create a new instance of a Gemini client
-    client = genai.Client(api_key=api_key)
+    generate_content(client, messages, verbose)
 
+    
+
+def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=messages
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        )
     )
 
     if verbose:
-        print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        print(response.text)
+
+    if not response.function_calls:
+        return response.text
+
+    for function_call_part in response.function_calls:
+        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
